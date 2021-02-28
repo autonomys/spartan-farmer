5ee195fe6cc2c589f13c531be33fcd30060f6d71
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2021-02-28 22:12:43 +0200

    Compatibility with latest experiments

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use jsonrpsee::common::Params;
-use serde::Deserialize;
 use jsonrpsee::client::Subscription;
+use jsonrpsee::common::Params;
+use serde::{Deserialize, Serialize};
+
+type SlotNumber = u64;
+
+#[derive(Debug, Serialize)]
+pub struct ProposedProofOfSpace {
+    slot_number: SlotNumber,
+}
 
 #[derive(Debug, Deserialize)]
 pub struct SlotInfo {
-    pub slot_number: u64,
-    pub epoch_randomness: Vec<u8>,
+    slot_number: SlotNumber,
+    epoch_randomness: Vec<u8>,
 }
 
 #[async_std::main]
@@ -13,14 +20,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::init();
 
     println!("Connecting");
-    let client = jsonrpsee::ws_client("ws://127.0.0.1:9944").await?;
+    let client = jsonrpsee::ws_client("ws://127.0.0.1:9945").await?;
     println!("Connected");
-    let mut sub: Subscription<SlotInfo> = client.subscribe("babe_subscribeSlotInfo", Params::None, "babe_unsubscribeSlotInfo").await?;
+    let mut sub: Subscription<SlotInfo> = client
+        .subscribe(
+            "babe_subscribeSlotInfo",
+            Params::None,
+            "babe_unsubscribeSlotInfo",
+        )
+        .await?;
     println!("Subscribed");
-    while let message = sub.next().await {
-        println!("{:?}", message);
-        client.request("babe_proposeProofOfSpace", Params::None).await?;
-    }
+    loop {
+        let slot_info = sub.next().await;
+        println!("{:?}", slot_info);
 
-    Ok(())
+        client
+            .notification(
+                "babe_proposeProofOfSpace",
+                Params::Array(vec![serde_json::to_value(&ProposedProofOfSpace {
+                    slot_number: slot_info.slot_number,
+                })
+                .unwrap()]),
+            )
+            .await;
+    }
 }
