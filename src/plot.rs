16fee055aac209493c6d2f7222f35f8786982020
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2021-04-04 23:31:19 +0300

    Fix tests after recent changes

--- src/plot.rs
+++ src/plot.rs
@@ -465,7 +465,7 @@ mod tests {
 
         let plot = Plot::open_or_create(&path).await.unwrap();
         assert_eq!(true, plot.is_empty().await);
-        plot.write(piece, index, salt).await.unwrap();
+        plot.write_many(vec![piece], index, salt).await.unwrap();
         assert_eq!(false, plot.is_empty().await);
         let extracted_piece = plot.read(index).await.unwrap();
 
@@ -492,74 +492,81 @@ mod tests {
         let salt: Salt = [1u8; 32];
 
         let plot = Plot::open_or_create(&path).await.unwrap();
-        for index in 0..1024 {
-            let piece = generate_random_piece();
-            plot.write(piece, index, salt).await.unwrap();
-        }
+
+        plot.write_many(
+            (0..1024_usize).map(|_| generate_random_piece()).collect(),
+            0,
+            salt,
+        )
+        .await
+        .unwrap();
 
         {
             let target = [0u8, 0, 0, 0, 0, 0, 0, 1];
             let solution_range =
                 u64::from_be_bytes([0u8, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]);
-            let solutions = plot.find_by_range(target, solution_range).await.unwrap();
             // This is probabilistic, but should be fine most of the time
-            assert!(!solutions.is_empty());
+            let (solution, _) = plot
+                .find_by_range(target, solution_range)
+                .await
+                .unwrap()
+                .unwrap();
             // Wraps around
             let lower = u64::from_be_bytes(target).wrapping_sub(solution_range / 2);
             let upper = u64::from_be_bytes(target) + solution_range / 2;
-            for (solution, _) in solutions {
-                let solution = u64::from_be_bytes(solution);
-                assert!(
-                    solution >= lower || solution <= upper,
-                    "Solution {:?} must be over wrapped lower edge {:?} or under upper edge {:?}",
-                    solution.to_be_bytes(),
-                    lower.to_be_bytes(),
-                    upper.to_be_bytes(),
-                );
-            }
+            let solution = u64::from_be_bytes(solution);
+            assert!(
+                solution >= lower || solution <= upper,
+                "Solution {:?} must be over wrapped lower edge {:?} or under upper edge {:?}",
+                solution.to_be_bytes(),
+                lower.to_be_bytes(),
+                upper.to_be_bytes(),
+            );
         }
 
         {
             let target = [0xff_u8, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xfe];
             let solution_range =
                 u64::from_be_bytes([0u8, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]);
-            let solutions = plot.find_by_range(target, solution_range).await.unwrap();
             // This is probabilistic, but should be fine most of the time
-            assert!(!solutions.is_empty());
+            let (solution, _) = plot
+                .find_by_range(target, solution_range)
+                .await
+                .unwrap()
+                .unwrap();
             // Wraps around
             let lower = u64::from_be_bytes(target) - solution_range / 2;
             let upper = u64::from_be_bytes(target).wrapping_add(solution_range / 2);
-            for (solution, _) in solutions {
-                let solution = u64::from_be_bytes(solution);
-                assert!(
-                    solution >= lower || solution <= upper,
-                    "Solution {:?} must be over lower edge {:?} or under wrapped upper edge {:?}",
-                    solution.to_be_bytes(),
-                    lower.to_be_bytes(),
-                    upper.to_be_bytes(),
-                );
-            }
+            let solution = u64::from_be_bytes(solution);
+            assert!(
+                solution >= lower || solution <= upper,
+                "Solution {:?} must be over lower edge {:?} or under wrapped upper edge {:?}",
+                solution.to_be_bytes(),
+                lower.to_be_bytes(),
+                upper.to_be_bytes(),
+            );
         }
 
         {
             let target = [0xef_u8, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff];
             let solution_range =
                 u64::from_be_bytes([0u8, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff, 0xff]);
-            let solutions = plot.find_by_range(target, solution_range).await.unwrap();
             // This is probabilistic, but should be fine most of the time
-            assert!(!solutions.is_empty());
+            let (solution, _) = plot
+                .find_by_range(target, solution_range)
+                .await
+                .unwrap()
+                .unwrap();
             let lower = u64::from_be_bytes(target) - solution_range / 2;
             let upper = u64::from_be_bytes(target) + solution_range / 2;
-            for (solution, _) in solutions {
-                let solution = u64::from_be_bytes(solution);
-                assert!(
-                    solution >= lower && solution <= upper,
-                    "Solution {:?} must be over lower edge {:?} and under upper edge {:?}",
-                    solution.to_be_bytes(),
-                    lower.to_be_bytes(),
-                    upper.to_be_bytes(),
-                );
-            }
+            let solution = u64::from_be_bytes(solution);
+            assert!(
+                solution >= lower && solution <= upper,
+                "Solution {:?} must be over lower edge {:?} and under upper edge {:?}",
+                solution.to_be_bytes(),
+                lower.to_be_bytes(),
+                upper.to_be_bytes(),
+            );
         }
 
         drop(plot);
