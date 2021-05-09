e8ee7b1b526333ec2648adbb93e00d8d75496f8d
Nazar Mokrynskyi <nazar@mokrynskyi.com>
2021-05-09 23:14:37 +0300

    Some more docs

--- src/plot.rs
+++ src/plot.rs
@@ -61,6 +61,12 @@ struct Inner {
     write_requests_sender: async_mpsc::Sender<WriteRequests>,
 }
 
+/// `Plot` struct is an abstraction on top of both plot and tags database. It converts async
+/// requests to internal reads/writes to the plot and tags database. It prioritizes reads over
+/// writes. Internally for that it has 2 queues for reads and writes requests, read requests are
+/// executed until exhausted after which at most 1 write request is handled and cycle repeats. This
+/// allows finding solution with as little delay as possible while introducing changes to the plot
+/// at the same time (re-plotting on salt changes or extending plot size).
 #[derive(Clone)]
 pub(crate) struct Plot {
     inner: Arc<Inner>,
@@ -300,6 +306,7 @@ impl Plot {
         })
     }
 
+    /// Whether plot doesn't have anything in it
     pub(crate) async fn is_empty(&self) -> bool {
         let (result_sender, result_receiver) = oneshot::channel();
 
@@ -340,6 +347,9 @@ impl Plot {
             .expect("Read encoding result sender was dropped")
     }
 
+    /// Find pieces within specified solution range.
+    ///
+    /// Returns tag and piece index.
     pub(crate) async fn find_by_range(
         &self,
         target: [u8; 8],
