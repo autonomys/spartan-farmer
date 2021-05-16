61572e679efd74efbf287d029670f581736d5ef2
Jeremiah Wagstaff <jeremiah.wagstaff@gmail.com>
2021-05-16 09:18:49 -0700

    switch to count by blocks instead of slots

--- src/commands/sim.rs
+++ src/commands/sim.rs
@@ -22,6 +22,7 @@ pub(crate) async fn sim(path: PathBuf) -> Result<(), Box<dyn std::error::Error>>
 
     // define initial plot size and solution range
     const EXPECTED_SLOTS_PER_BLOCK: u64 = 6;
+    const BLOCKS_PER_ERA: u64 = 2016;
     const SLOTS_PER_ERA: u64 = 2016 * EXPECTED_SLOTS_PER_BLOCK;
     const ERA_TRIALS: u64 = 100;
     const SLOT_TRIALS: u64 = SLOTS_PER_ERA * ERA_TRIALS;
@@ -31,10 +32,12 @@ pub(crate) async fn sim(path: PathBuf) -> Result<(), Box<dyn std::error::Error>>
     let mut challenge = rand::thread_rng().gen::<[u8; PRIME_SIZE_BYTES]>();
     let mut solution_range = INITIAL_SOLUTION_RANGE;
     let mut era_solution_count: u64 = 0;
+    let mut era_slot_count: u64 = 0;
     let mut era = 0;
 
     for i in 0..SLOT_TRIALS {
         debug!("New slot: {:?}", i);
+        era_slot_count += 1;
 
        match plot
             .find_by_range(challenge, solution_range)
@@ -53,15 +56,15 @@ pub(crate) async fn sim(path: PathBuf) -> Result<(), Box<dyn std::error::Error>>
         challenge = hash_challenge(challenge);
 
         // check for era boundary
-        if (i + 1) % SLOTS_PER_ERA == 0 {
+        if era_solution_count > 0 && era_solution_count % BLOCKS_PER_ERA == 0 {
             // update the solution range
             let actual_slots_per_block = if era_solution_count == 0 {
                 0f64
             } else {
-                (SLOTS_PER_ERA as f64 / era_solution_count as f64) as f64
+                (era_slot_count as f64 / era_solution_count as f64) as f64
             };
             let adjustment_factor: f64 = actual_slots_per_block / EXPECTED_SLOTS_PER_BLOCK as f64;
-            solution_range = (solution_range as f64 * adjustment_factor) as u64;
+            solution_range = (solution_range as f64 * adjustment_factor).round() as u64;
 
             era += 1;
 
@@ -72,6 +75,7 @@ pub(crate) async fn sim(path: PathBuf) -> Result<(), Box<dyn std::error::Error>>
             info!("Adjustment factor: {} \n", adjustment_factor);
 
             era_solution_count = 0;
+            era_slot_count = 0;
         }
     }
 
